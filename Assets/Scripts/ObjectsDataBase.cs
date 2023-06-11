2f6f1882f2f27bc1d6f884c0740425f2f891e842
philirmscher <82280738+philirmscher@users.noreply.github.com>
2023-06-12 01:37:44 +0200

    created ObjectsDatabase Class
    This will manage all Objects the Player can place

---
+++ Assets/Scripts/ObjectsDataBase.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu] public class ObjectsDataBase : ScriptableObject
+{
+    public List<ObjectsData> objectsData;
+
+}
+
+[Serializable] public class ObjectsData
+{
+
+    [field: SerializeField] public string Name { get; private set; }
+    [field: SerializeField] public int ID { get; private set; }
+    [field: SerializeField] public Vector2Int Size { get; private set; } = Vector2Int.one;
+    [field: SerializeField] public GameObject Prefab { get; private set; }
+}
