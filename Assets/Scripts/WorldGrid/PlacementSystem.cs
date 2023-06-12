2fb00d4a954c12b807e1a073413d7b23e73de46d
philirmscher <82280738+philirmscher@users.noreply.github.com>
2023-06-12 09:49:59 +0200

    added Towerplaceholder + UI to select it
    Tower is in a Database with ID 1

--- Assets/Scripts/WorldGrid/PlacementSystem.cs
+++ Assets/Scripts/WorldGrid/PlacementSystem.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,8 +8,55 @@ public class PlacementSystem : MonoBehaviour
     [SerializeField] private GameObject mouseIndicator, cellIndicator;
     [SerializeField] private InputManager inputManager;
     [SerializeField] private Grid worldGrid;
+    [SerializeField] private ObjectsDataBase database;
+    private int selectedObjectIndex = -1;
+    [SerializeField] private GameObject gridVisualization;
+
+    private void Start()
+    {
+        StopPlacement();
+    }
+
+    public void StartPlacment(int ID)
+    {
+        Debug.Log("Placed!");
+        selectedObjectIndex = database.objectsData.FindIndex(data => data.ID == ID);
+        if(selectedObjectIndex < 0)
+        {
+            Debug.LogError($"No ID Found {ID}");
+            return;
+        }
+        gridVisualization.SetActive(true);
+        cellIndicator.SetActive(true);
+        inputManager.OnClicked += PlaceStructure;
+        inputManager.OnExit += StopPlacement;
+    }
+
+    private void PlaceStructure()
+    {
+        if (inputManager.IsPointerOverUI())
+            return;
+
+        Vector3 mousePosition = inputManager.GetSelectedMapPosition();
+        Vector3Int worldGridPosition = worldGrid.WorldToCell(mousePosition);
+        GameObject newObject = Instantiate(database.objectsData[selectedObjectIndex].Prefab);
+        newObject.transform.position = worldGrid.CellToWorld(worldGridPosition);
+    }
+
+    private void StopPlacement()
+    {
+        selectedObjectIndex = -1;
+        gridVisualization.SetActive(false);
+        cellIndicator.SetActive(false);
+        inputManager.OnClicked -= PlaceStructure;
+        inputManager.OnExit -= StopPlacement;
+    }
+
     private void Update()
     {
+        if (selectedObjectIndex < 0)
+            return;
+
         Vector3 mousePosition = inputManager.GetSelectedMapPosition();
         Vector3Int worldGridPosition = worldGrid.WorldToCell(mousePosition);
         mouseIndicator.transform.position = mousePosition;
