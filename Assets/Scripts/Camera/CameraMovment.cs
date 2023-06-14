f099cb662222332d485252dd4a763b6bc135d7a2
philirmscher <82280738+philirmscher@users.noreply.github.com>
2023-06-14 23:35:18 +0200

    camera rework
    The camera fokuses now on a moving object which can be moved by the player with "WASD" keys. The is a collider placed around the worldgrid where the camera can´t move.

--- Assets/Scripts/Camera/CameraMovment.cs
+++ Assets/Scripts/Camera/CameraMovment.cs
@@ -3,16 +3,25 @@ using UnityEngine;
 public class CameraMovement : MonoBehaviour
 {
     [SerializeField] private Camera cam;
-    [SerializeField] private Transform target;
-    [SerializeField] private float distanceToTarget = 10;
+    [SerializeField] private GameObject target;
+    [SerializeField] private float zoomHeight = 10;
     [SerializeField] private float zoomSteps = 1;
     [SerializeField] private float maxZoomDistance = 30;
     [SerializeField] private float minZoomDistance = 7;
     [SerializeField] private float maxXAngle = 80f;
     [SerializeField] private float minXAngle = 15f;
+    [SerializeField] private float movementSpeed = 10f;
+    [SerializeField] private LayerMask cameraCollider;
     private Vector3 previousPosition;
 
     private void Update()
+    {
+        HandleCameraRotation();
+        HandleCameraZoom();
+        HandleCameraMovement();
+    }
+
+    private void HandleCameraRotation()
     {
         if (Input.GetMouseButtonDown(2))
         {
@@ -26,48 +35,82 @@ public class CameraMovement : MonoBehaviour
             float rotationAroundYAxis = -direction.x * 180; // camera moves horizontally
             float rotationAroundXAxis = direction.y * 180; // camera moves vertically
 
-            // Begrenze die X-Rotation zwischen 0 und 90 Grad
+            // Limit the X-rotation between 0 and 90 degrees
             float currentXRotation = cam.transform.rotation.eulerAngles.x;
             float angle = maxXAngle;
             float clampedXRotation = Mathf.Clamp(currentXRotation + rotationAroundXAxis, 0f, angle);
             rotationAroundXAxis = clampedXRotation - currentXRotation;
 
-            cam.transform.position = target.position;
+            cam.transform.position = target.transform.position;
             cam.transform.Rotate(new Vector3(1, 0, 0), rotationAroundXAxis);
             cam.transform.Rotate(new Vector3(0, 1, 0), rotationAroundYAxis, Space.World);
 
-            cam.transform.Translate(new Vector3(0, 0, -distanceToTarget));
+            cam.transform.Translate(new Vector3(0, 0, -zoomHeight));
 
             previousPosition = newPosition;
         }
+    }
 
-        if (Input.GetAxis("Mouse ScrollWheel") < 0)
+    private void HandleCameraZoom()
+    {
+        float zoomInput = Input.GetAxis("Mouse ScrollWheel");
+
+        zoomHeight -= zoomInput * zoomSteps;
+        zoomHeight = Mathf.Clamp(zoomHeight, minZoomDistance, maxZoomDistance);
+
+        Vector3 newPosition = target.transform.position - cam.transform.forward * zoomHeight;
+
+        Collider[] colliders = Physics.OverlapSphere(newPosition, 0.5f, cameraCollider);
+        bool collided = false;
+        foreach (Collider collider in colliders)
         {
-            distanceToTarget += zoomSteps;
-            if(distanceToTarget >= maxZoomDistance)
+            if (collider.CompareTag("CameraCollider"))
             {
-                distanceToTarget = maxZoomDistance;
-                cam.transform.position = target.position;
-                cam.transform.Translate(new Vector3(0, 0, -distanceToTarget));
-            }
-            else
-            {
-                cam.transform.Translate(new Vector3(0, 0, -zoomSteps));
+                collided = true;
+                break;
             }
         }
-        if (Input.GetAxis("Mouse ScrollWheel") > 0)
+
+        if (!collided)
         {
-            distanceToTarget -= zoomSteps;
-            if (distanceToTarget <= minZoomDistance)
+            cam.transform.position = newPosition;
+        }
+    }
+
+    private void HandleCameraMovement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        // Get camera's forward direction on the x-z plane
+        Vector3 forwardDirection = cam.transform.forward;
+        forwardDirection.y = 0;
+        forwardDirection.Normalize();
+
+        // Get camera's right direction on the x-z plane
+        Vector3 rightDirection = cam.transform.right;
+        rightDirection.y = 0;
+        rightDirection.Normalize();
+
+        // Calculate movement vector
+        Vector3 movement = (forwardDirection * verticalInput + rightDirection * horizontalInput) * movementSpeed * Time.deltaTime;
+        Vector3 newPosition = target.transform.position + movement;
+
+        // Check for collision with CameraCollider objects
+        Collider[] colliders = Physics.OverlapSphere(newPosition, 0.5f, cameraCollider);
+        bool collided = false;
+        foreach (Collider collider in colliders)
+        {
+            if (collider.CompareTag("CameraCollider"))
             {
-                distanceToTarget = minZoomDistance;
-                cam.transform.position = target.position;
-                cam.transform.Translate(new Vector3(0, 0, -distanceToTarget));
+                collided = true;
+                break;
             }
-            else
-            {
-                cam.transform.Translate(new Vector3(0, 0, +zoomSteps));
-            }
+        }
+
+        if (!collided)
+        {
+            target.transform.position = newPosition;
         }
     }
 }
