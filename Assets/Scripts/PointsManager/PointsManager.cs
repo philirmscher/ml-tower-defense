a1ceb5cfc15f5e12f000bedbabf45f18e00cdbaf
Philipp Irmscher <82280738+philirmscher@users.noreply.github.com>
2023-06-17 11:23:20 +0200

    added simple Pointsmanager
    functions to add and get points

--- Assets/Scripts/PointsManager/PointsManager.cs
+++ Assets/Scripts/PointsManager/PointsManager.cs
@@ -5,4 +5,19 @@ using UnityEngine;
 public class PointsManager : MonoBehaviour
 {
     [SerializeField] private int points;
+
+    public PointsManager(int startPoints)
+    {
+        this.points = startPoints;
+    }
+
+    public int GetCurrentPoints()
+    {
+        return points;
+    }
+
+    public void AddPoints(int points)
+    {
+        this.points += points;
+    }
 }
